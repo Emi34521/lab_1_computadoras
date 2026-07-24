997cacd15b0dffaf353513ba993f679b7bb72592
Emi34521 <cumatzemilio6@gmail.com>
2026-07-24 16:18:00 -0600

    added the fuction to draw polygons and changed the size of the screen

--- src/framebuffer.zig
+++ src/framebuffer.zig
@@ -1,5 +1,10 @@
 const rl = @import("raylib");
 
+pub const Point = struct {
+    x: f32,
+    y: f32,
+};
+
 pub const Framebuffer = struct {
     width: i32,
     height: i32,
@@ -16,19 +21,29 @@ pub const Framebuffer = struct {
             .texture = null,
         };
     }
+    pub fn draw_polygon(self: *Framebuffer, points: []const Point, color: rl.Color) void {
+        if (points.len < 2) return;
+        for (points, 0..) |p, i| {
+            const next = points[(i + 1) % points.len]; // conecta el último punto con el primero
+            self.draw_line(p, next, color);
+        }
+    }
 
-pub fn draw_line(self: Framebuffer, x0: i32, y0: i32, x1: i32, y1: i32, color: rl.Color) void {
-        var x = x0;
-        var y = y0;
-        const dx: i32 = @intCast(@abs(x0 - x1));
-        const dy: i32 = -@as(i32, @intCast(@abs(y0 - y1)));
-        const step_x: i32 = if (x0 < x1) 1 else -1;
-        const step_y: i32 = if (y0 < y1) 1 else -1;
+    pub fn draw_line(self: *Framebuffer, p0: Point, p1: Point, color: rl.Color) void {
+        var x: i32 = @intFromFloat(p0.x);
+        var y: i32 = @intFromFloat(p0.y);
+        const x1: i32 = @intFromFloat(p1.x);
+        const y1: i32 = @intFromFloat(p1.y);
+
+        const dx: i32 = @intCast(@abs(x - x1));
+        const dy: i32 = -@as(i32, @intCast(@abs(y - y1)));
+        const step_x: i32 = if (x < x1) 1 else -1;
+        const step_y: i32 = if (y < y1) 1 else -1;
 
         var err: isize = dx + dy;
         while (true) {
             if (x >= 0 and y >= 0 and x < self.width and y < self.height) {
-                self.draw_pixel(x, y, color) catch unreachable;
+                self.draw_pixel(@floatFromInt(x), @floatFromInt(y), color) catch unreachable;
             }
 
             if (x == x1 and y == y1) break;
@@ -51,7 +66,6 @@ pub fn draw_line(self: Framebuffer, x0: i32, y0: i32, x1: i32, y1: i32, color: r
 
         if (i32_x > self.width) return error.BadCoordinates;
         if (i32_y > self.height) return error.BadCoordinates;
-
         if (i32_x < 0) return error.BadCoordinates;
         if (i32_y < 0) return error.BadCoordinates;
 
@@ -60,7 +74,6 @@ pub fn draw_line(self: Framebuffer, x0: i32, y0: i32, x1: i32, y1: i32, color: r
 
     pub fn clear(self: *Framebuffer) void {
         self.image.clearBackground(self.background_color);
-        // Si no ponemos esto, se muere la compu. Pruebenlo, pero ahí le dan ctrl+c
         if (self.texture) |texture| {
             rl.unloadTexture(texture);
         }
@@ -72,9 +85,5 @@ pub fn draw_line(self: Framebuffer, x0: i32, y0: i32, x1: i32, y1: i32, color: r
 
         self.texture = try rl.loadTextureFromImage(self.image);
         rl.drawTexture(self.texture.?, 0, 0, .white);
-        //equivalente a
-        // if (self.texture) |texture| {
-        //     rl.drawTexture(texture, 0, 0, .white);
-        // } else unreachable;
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -2,58 +2,53 @@ const std = @import("std");
 const rl = @import("raylib");
 const fb = @import("framebuffer.zig");
 
-const Clock = std.Io.Clock.real;
+const Point = fb.Point;
 
 pub fn main(init: std.process.Init) !void {
-    const width = 800;
-    const height = 600;
+    _ = init;
+    const width = 1920;
+    const height = 1080;
     var framebuffer = fb.Framebuffer.init(width, height, rl.Color.black);
 
     rl.initWindow(width, height, "Software Renderer");
     defer rl.closeWindow();
     rl.setTargetFPS(60);
 
-    const start = Clock.now(init.io);
-    var frame_start = start;
-    var dt: f32 = 1;
+    const polygon1 = [_]Point{
+        .{ .x = 165, .y = 380 }, .{ .x = 185, .y = 360 }, .{ .x = 180, .y = 330 },
+        .{ .x = 207, .y = 345 }, .{ .x = 233, .y = 330 }, .{ .x = 230, .y = 360 },
+        .{ .x = 250, .y = 380 }, .{ .x = 220, .y = 385 }, .{ .x = 205, .y = 410 },
+        .{ .x = 193, .y = 383 },
+    };
+
+    const polygon2 = [_]Point{
+        .{ .x = 321, .y = 335 }, .{ .x = 288, .y = 286 }, .{ .x = 339, .y = 251 }, .{ .x = 374, .y = 302 },
+    };
+
+    const polygon3 = [_]Point{
+        .{ .x = 377, .y = 249 }, .{ .x = 411, .y = 197 }, .{ .x = 436, .y = 249 },
+    };
+
+    const polygon4 = [_]Point{
+        .{ .x = 413, .y = 177 }, .{ .x = 448, .y = 159 }, .{ .x = 502, .y = 88 },  .{ .x = 553, .y = 53 },
+        .{ .x = 535, .y = 36 },  .{ .x = 676, .y = 37 },  .{ .x = 660, .y = 52 },  .{ .x = 750, .y = 145 },
+        .{ .x = 761, .y = 179 }, .{ .x = 672, .y = 192 }, .{ .x = 659, .y = 214 }, .{ .x = 615, .y = 214 },
+        .{ .x = 632, .y = 230 }, .{ .x = 580, .y = 230 }, .{ .x = 597, .y = 215 }, .{ .x = 552, .y = 214 },
+        .{ .x = 517, .y = 144 }, .{ .x = 466, .y = 180 },
+    };
+
+    const polygon5 = [_]Point{
+        .{ .x = 682, .y = 175 }, .{ .x = 708, .y = 120 }, .{ .x = 735, .y = 148 }, .{ .x = 739, .y = 170 },
+    };
 
     while (!rl.windowShouldClose()) {
-        frame_start = Clock.now(init.io);
-        defer {
-            dt = @floatFromInt(frame_start.durationTo(Clock.now(init.io)).toNanoseconds());
-            dt /= 1_000_000_000;
-        }
-        // const time = start.durationTo(Clock.now(init.io)).toMicroseconds();
-        // std.debug.print("{}\n", .{dt});
-        framebuffer.clear(); // Qué pasa si no incluimos esta linea?
+        framebuffer.clear();
 
-        // framebuffer.image.drawCircle(@intFromFloat(x), height / 2 - 7, 15, .red);
-        const elapsed: f32 = @floatFromInt(start.durationTo(Clock.now(init.io)).toNanoseconds());
-        const elapsed_micro: f32 = elapsed / 1_000;
-        const elapsed_mili: f32 = elapsed / 1_000_000;
-        const elapsed_seconds: f32 = elapsed / 1_000_000_000;
-        _ = elapsed_seconds;
-        _ = elapsed_micro;
-
-        const speed = 0.25;
-        const periods = 10;
-
-        var x_last: f32 = 0;
-        var y_last: f32 = @sin((elapsed_mili * speed) * std.math.tau * periods / width) * height / 3 + height / 2;
-        for (0..width) |i| {
-            const x: f32 = @floatFromInt(i);
-            const y = @sin((x + elapsed_mili * speed) * std.math.tau * periods / width) * height / 3 + height / 2;
-
-            framebuffer.draw_line(
-                .{ .x = x_last, .y = y_last },
-                .{ .x = x, .y = y },
-                .{ .r = 0xff, .g = 0, .b = 0, .a = 0xf0 },
-            );
-
-            defer x_last = x;
-            defer y_last = y;
-            // std.debug.print("elapsed: {} x:{} y:{} x_last:{} y_last:{}\n", .{ elapsed, x, y, x_last, y_last });
-        }
+        framebuffer.draw_polygon(&polygon1, .white);
+        framebuffer.draw_polygon(&polygon2, .white);
+        framebuffer.draw_polygon(&polygon3, .white);
+        framebuffer.draw_polygon(&polygon4, .white);
+        framebuffer.draw_polygon(&polygon5, .white);
 
         try framebuffer.swap();
     }
